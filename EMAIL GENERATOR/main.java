aa4c3a0c9f56d289ea5aed702e76a36d41e1881b
Sayak Hajra <110591782+Sayak459@users.noreply.github.com>
2024-06-30 10:49:58 +0530

    Add files via upload

---
+++ EMAIL GENERATOR/main.java
@@ -0,0 +1,11 @@
+
+package emailapp;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+
+        Email emi = new Email("John", "Doe");
+        System.out.println(emi.showInfo());
+    }
+}
